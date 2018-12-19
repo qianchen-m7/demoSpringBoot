5100421f4092745281ba20cc37cd4294d27f48d5
chen_qian <chen_qian@baofu.com>
2018-12-19 15:17:32 +0800

    。。。

--- src/test/java/com/example/demoSpringBoot/DataTest.java
+++ src/test/java/com/example/demoSpringBoot/DataTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by BF100499 on 2018/11/6.
@@ -40,5 +42,22 @@ public class DataTest {
         User user = UserMapper.getOne(1l);
         System.out.println(user.toString());
 ;
+    }
+
+    @Test
+    public void testas(){
+        String s="调整1001-1031上海jgkhg;gjvbh暂fg";
+        String reg="(?<=[0-9]+(?=[^0-9]*$)).+(?=暂)";
+        Pattern r = Pattern.compile(reg);
+
+        Matcher m = r.matcher(s);
+        int i = 0;
+        while (m.find()) {
+            // group(0)或group()将会返回整个匹配的字符串（完全匹配）；group(i)则会返回与分组i匹配的字符
+            // 这个例子只有一个分组
+            System.out.println(m.group(0) );
+
+        }
+
     }
 }
