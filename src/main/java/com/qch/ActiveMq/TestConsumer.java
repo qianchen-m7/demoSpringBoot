c1d1b5a4c82bab9d1b0481ef1d0e434f5e373c96
chen_qian <chen_qian@baofu.com>
2019-02-26 14:49:42 +0800

    消息队列ActiveMQ

---
+++ src/main/java/com/qch/ActiveMq/TestConsumer.java
@@ -0,0 +1,35 @@
+package com.qch.ActiveMq;
+
+/**
+ * Created by BF100499 on 2019/2/26.
+ */
+public class TestConsumer {
+    public static void main(String[] args){
+        Comsumer comsumer = new Comsumer();
+        comsumer.init();
+        TestConsumer testConsumer = new TestConsumer();
+        new Thread(testConsumer.new ConsumerMq(comsumer)).start();
+        new Thread(testConsumer.new ConsumerMq(comsumer)).start();
+        new Thread(testConsumer.new ConsumerMq(comsumer)).start();
+        new Thread(testConsumer.new ConsumerMq(comsumer)).start();
+    }
+
+    private class ConsumerMq implements Runnable{
+        Comsumer comsumer;
+        public ConsumerMq(Comsumer comsumer){
+            this.comsumer = comsumer;
+        }
+
+        @Override
+        public void run() {
+            while(true){
+                try {
+                    comsumer.getMessage("Jaycekon-MQ");
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/qch/ActiveMq/TestMq.java
@@ -0,0 +1,46 @@
+package com.qch.ActiveMq;
+
+/**
+ * Created by BF100499 on 2019/2/26.
+ */
+public class TestMq {
+    public static void main(String[] args){
+        Producter producter = new Producter();
+        producter.init();
+        TestMq testMq = new TestMq();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //Thread 1
+        new Thread(testMq.new ProductorMq(producter)).start();
+        //Thread 2
+        new Thread(testMq.new ProductorMq(producter)).start();
+        //Thread 3
+        new Thread(testMq.new ProductorMq(producter)).start();
+        //Thread 4
+        new Thread(testMq.new ProductorMq(producter)).start();
+        //Thread 5
+        new Thread(testMq.new ProductorMq(producter)).start();
+    }
+
+    private class ProductorMq implements Runnable{
+        Producter producter;
+        public ProductorMq(Producter producter){
+            this.producter = producter;
+        }
+
+        @Override
+        public void run() {
+            while(true){
+                try {
+                    producter.sendMessage("Jaycekon-MQ");
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
